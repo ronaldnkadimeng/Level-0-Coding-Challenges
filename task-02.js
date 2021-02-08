eed4704b4ba70faa4dede65dce791677fd70b3e6
ronaldnkadimeng <ronaldnkadimeng5@gmail.com>
2021-02-08 18:19:17 +0100

    Added task-02.js

---
+++ task-02.js
@@ -0,0 +1,17 @@
+var x = 0;
+var y = 0;
+var z = 0;
+var a = 0;
+var b = 0;
+
+x = 1 + 1 * 2;
+y = (1 + 1) * 2;
+z = 1 + 1 * 2;
+a = 1 + (1 * 2) / 2;
+b = (1 + 1 * 2) / 2;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
