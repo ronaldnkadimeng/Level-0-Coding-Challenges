b4a40664c4cdaec9a97464180a198f2ae1166580
ronaldnkadimeng <ronaldnkadimeng5@gmail.com>
2021-02-08 18:25:27 +0100

    Added task-08.js

---
+++ task-08.js
@@ -0,0 +1,22 @@
+let number = 134;
+
+function convertToTime(number) {
+  let stringMinute = "";
+  let stringHour = "";
+  let hour = Math.floor(number / 60);
+  let minute = number % 60;
+
+  if (hour === 1) {
+    stringHour = "hour";
+  } else {
+    stringHour = "hours";
+  }
+  if (minute === 1) {
+    stringMinute = "minute";
+  } else {
+    stringMinute = "minutes";
+  }
+  return hour + " " + stringHour + ", " + minute + " " + stringMinute;
+}
+
+console.log(convertToTime(number));
