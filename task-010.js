bfda4f83de424a75529d34407b9ab873f5ed185b
ronaldnkadimeng <ronaldnkadimeng5@gmail.com>
2021-02-09 21:32:19 +0100

    modified for-loop

--- task-010.js
+++ task-010.js
@@ -1,13 +1,16 @@
 let string1 = "house";
-let string2 = "computers";
+let string2 = "computer";
 
 function findCommonCharacters(string1, string2) {
-  const MAX_CHAR = 26;
-  for (var begin = 0; begin < MAX_CHAR; begin++) {
+  let wordLength = Math.max(string1.length, string2.length);
+  let commonCharacters = [];
+  let count = 0;
+  for (let begin = 0; begin < wordLength; begin++) {
     if (string1.indexOf(string2[begin]) !== -1) {
-      console.log(string2[begin] + ",");
+      commonCharacters[count] = string2[begin];
+      count++;
     }
   }
+  console.log(commonCharacters.join());
 }
-
 findCommonCharacters(string1, string2);
