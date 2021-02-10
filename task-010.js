ca8054c8d2b7123ffd6982901b27206752993afe
ronaldnkadimeng <ronaldnkadimeng5@gmail.com>
2021-02-10 18:22:45 +0100

    inserted nested for-loop and removed global variables

--- task-010.js
+++ task-010.js
@@ -1,16 +1,10 @@
-let string1 = "house";
-let string2 = "computer";
-
 function findCommonCharacters(string1, string2) {
-  let wordLength = Math.max(string1.length, string2.length);
   let commonCharacters = [];
-  let count = 0;
-  for (let begin = 0; begin < wordLength; begin++) {
-    if (string1.indexOf(string2[begin]) !== -1) {
-      commonCharacters[count] = string2[begin];
-      count++;
+  for (let begin = 0; begin < string1.length; begin++) {
+    for (let letter = 0; letter < string2.length; letter++) {
+      if (string1[begin] == string2[letter])
+        commonCharacters.push(string2[letter]);
     }
   }
-  console.log(commonCharacters.join());
+  console.log(commonCharacters.join(","));
 }
-findCommonCharacters(string1, string2);
